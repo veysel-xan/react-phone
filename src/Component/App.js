7a74fe2ccf7ef1e340f0fcfaca735093eb094f3e
veysel-xan <veysel@webzool.com>
2020-03-22 10:59:27 +0400

    first

---
+++ src/Component/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+import Contacts from "./Contacts";
+
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.addContact = this.addContact.bind(this)
+    }
+    state = {
+        contacts: [
+            {
+                name: "Veysel",
+                Phone:546544654
+            },
+            {
+                name: "Cavidan",
+                Phone:78978789
+            }
+
+        ]
+    };
+  addContact(contact) {
+    const {contacts} = this.state;
+      contacts.push(contact)
+      this.setState({
+          contacts
+      })
+  }
+    render() {
+    return(
+        <div className="App">
+         <Contacts
+             addContact={this.addContact}
+             contacts={this.state.contacts} />
+        </div>
+    )
+  }
+}
+
+export default App;
